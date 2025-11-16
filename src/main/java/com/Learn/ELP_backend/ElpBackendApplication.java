6c96914ccac4cc789775e038b768b7460744b106
vishwaprabhathiyarajarathne@gmail.com <vishwaprabhathiyarajarathne@gmail.com>
2025-11-16 12:10:45 +0530

    enable role base access to APIs

--- src/main/java/com/Learn/ELP_backend/ElpBackendApplication.java
+++ src/main/java/com/Learn/ELP_backend/ElpBackendApplication.java
@@ -2,8 +2,10 @@ package com.Learn.ELP_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true)
 public class ElpBackendApplication {
 
 	public static void main(String[] args) {
