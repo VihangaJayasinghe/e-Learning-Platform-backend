0670f8515a3e545b09c8df20a88c60b23766e1e2
Chamika Senarathna <chamikasenarathna2001@gmail.com>
2025-11-21 14:16:14 +0530

    Added models for questions and quizes

---
+++ src/main/java/com/Learn/ELP_backend/model/Quiz.java
@@ -0,0 +1,35 @@
+package com.Learn.ELP_backend.model;
+
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@lombok.Builder
+@Document(collection = "quizes")
+public class Quiz {
+    @Id
+    private String Id;
+    
+    private String classId;
+    private YearMonth yearMonth;
+    private String quizTitle;
+    private String description;
+    private List<Question> questions;
+    // timeLimit
+    private String createdBy;
+    
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+
+}
