5b4e0dff328b37a9160fa085bcc72d884c9812ea
Chamika Senarathna <chamikasenarathna2001@gmail.com>
2025-11-21 14:17:10 +0530

    Implemented quiz services inside ClassServices

--- src/main/java/com/Learn/ELP_backend/service/ClassService.java
+++ src/main/java/com/Learn/ELP_backend/service/ClassService.java
@@ -2,6 +2,7 @@ package com.Learn.ELP_backend.service;
 
 import com.Learn.ELP_backend.model.Class;
 import com.Learn.ELP_backend.model.ClassStatus;
+import com.Learn.ELP_backend.model.Quiz;
 
 import java.util.List;
 
@@ -29,4 +30,10 @@ public interface ClassService {
     
     // Get specific month's videos
     List<String> getMonthVideos(String classId, String yearMonth);
+
+    // Add, update, delete quiz
+    Quiz addQuizToMonth(String classId, String YearMonth, Quiz quiz);
+    Quiz updateQuiz(String classId, String yearmonth, String quizId, Quiz quizUpdate);
+    void deleteQuiz(String classId, String yearMonth, String quizId);
+
 }
